2fd8872bc6c078497e3b1489d7422316e046429f
Festino <offestashka@mail.ru>
2023-12-14 13:21:18 +0300

    feat: add AudioInfo

---
+++ DiscordMusicBot/Services/Discord/AudioInfo.cs
@@ -0,0 +1,11 @@
+﻿using DiscordMusicBot.AudioRequesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot.Services.Discord
+{
+    public record AudioInfo(Video Video, TimeSpan CurrentTime);
+}
