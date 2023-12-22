a2bd56041580bc6b0b0a325a94cdb1615ec52342
Festino <offestashka@mail.ru>
2023-12-22 16:18:55 +0300

    feat: add UnknownCommandExecutor

---
+++ DiscordMusicBot/Commands/Executors/UnknownCommandExecutor.cs
@@ -0,0 +1,20 @@
+﻿using DiscordMusicBot.Abstractions;
+using DiscordMusicBot.Services.Discord;
+
+namespace DiscordMusicBot.Commands.Executors
+{
+    public class UnknownCommandExecutor : ICommandExecutor
+    {
+        private readonly INotificationService _notificationService;
+
+        public UnknownCommandExecutor(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task ExecuteAsync(string args, DiscordMessageInfo discordMessageInfo)
+        {
+            await _notificationService.SendAsync(new CommandResponse(CommandResponseStatus.Ok, "unknown command"));
+        }
+    }
+}
