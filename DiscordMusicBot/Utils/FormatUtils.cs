ca098089da5b87af6f6f0135221f6797b5086aa6
Festino <offestashka@mail.ru>
2023-12-25 12:38:26 +0300

    refactor: move format methods

--- DiscordMusicBot/Utils/FormatUtils.cs
+++ DiscordMusicBot/Utils/FormatUtils.cs
@@ -11,5 +11,23 @@ namespace DiscordMusicBot.Utils
             string fullTimeStr = fullTime.ToString(formatStr);
             return Format.Code($"{currentTimeStr} / {fullTimeStr}");
         }
+
+        public static string FormatProgressBar(double progress, int cellsCount, string cellStates)
+        {
+            if (cellsCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(cellsCount));
+
+            int n = cellStates.Length - 1;
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException(nameof(cellStates) + ".Length");
+
+            int stateProgress = (int)Math.Floor(progress * cellsCount * n);
+            int completedCellsCount = stateProgress / n;
+            char currentCell = cellStates[stateProgress % n];
+
+            string completedCells = new string(cellStates[^1], completedCellsCount);
+            string emptyCells = new string(cellStates[0], cellsCount - completedCellsCount - 1);
+            return completedCells + currentCell + emptyCells;
+        }
     }
 }
---
+++ DiscordMusicBot/Utils/MessageFormatUtils.cs
@@ -0,0 +1,19 @@
+﻿using DiscordMusicBot.Abstractions;
+
+namespace DiscordMusicBot.Utils
+{
+    public class MessageFormatUtils
+    {
+        private const string CellStates = "░▒▓█";
+        private const int ProgressBarLength = 30;
+
+        public static string? FormatPlayingMessage(AudioInfo audioInfo)
+        {
+            double progress = audioInfo.CurrentTime.TotalSeconds / audioInfo.Video.Header.Duration.TotalSeconds;
+            string timeBar = FormatUtils.FormatProgressBar(progress, ProgressBarLength, CellStates);
+
+            string timeStr = FormatUtils.FormatTimestamps(audioInfo.CurrentTime, audioInfo.Video.Header.Duration);
+            return string.Format("Playing {0}\n{1} {2}", audioInfo.Video.Header.Title, timeBar, timeStr);
+        }
+    }
+}
