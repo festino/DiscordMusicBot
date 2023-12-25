c4dd64a8337b21839562af9d982050aa30d24ccc
Festino <offestashka@mail.ru>
2023-12-25 11:06:08 +0300

    feat: add more progress bar states

--- DiscordMusicBot/Services/Discord/FloatingMessageController.cs
+++ DiscordMusicBot/Services/Discord/FloatingMessageController.cs
@@ -5,6 +5,7 @@ namespace DiscordMusicBot.Services.Discord
 {
     public class FloatingMessageController : IFloatingMessageController
     {
+        private const string CellStates = "░▒▓█";
         private const int ProgressBarLength = 30;
         private const int UpdateDelayMs = 1000;
 
@@ -22,15 +23,16 @@ namespace DiscordMusicBot.Services.Discord
         {
             while (true)
             {
-                int delayMs = UpdateDelayMs;
+                var timeStart = DateTime.Now;
+
                 AudioInfo? audioInfo = _audioStreamer.GetPlaybackInfo();
                 if (audioInfo is not null)
                 {
-                    var timeStart = DateTime.Now;
                     await UpdateTimeAsync(audioInfo);
-                    int msPassed = (int)(DateTime.Now - timeStart).TotalMilliseconds;
-                    delayMs = Math.Max(0, delayMs - msPassed);
                 }
+
+                int msPassed = (int)(DateTime.Now - timeStart).TotalMilliseconds;
+                int delayMs = Math.Max(0, UpdateDelayMs - msPassed);
                 await Task.Delay(delayMs);
             }
         }
@@ -38,11 +40,29 @@ namespace DiscordMusicBot.Services.Discord
         private async Task UpdateTimeAsync(AudioInfo audioInfo)
         {
             double progress = audioInfo.CurrentTime.TotalSeconds / audioInfo.Video.Header.Duration.TotalSeconds;
-            int completedCells = (int)Math.Floor(progress * ProgressBarLength);
-            string timeBar = new string('█', completedCells) + new string('░', ProgressBarLength - completedCells);
+            string timeBar = GetProgressBar(progress, ProgressBarLength, CellStates);
+
             string timeStr = FormatUtils.FormatTimestamps(audioInfo.CurrentTime, audioInfo.Video.Header.Duration);
             string message = string.Format("Playing {0}\n{1} {2}", audioInfo.Video.Header.Title, timeBar, timeStr);
             await _floatingMessage.UpdateAsync(message);
+        }
+
+        private static string GetProgressBar(double progress, int cellsCount, string cellStates)
+        {
+            if (cellsCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(cellsCount));
+
+            int n = cellStates.Length - 1;
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException(nameof(cellStates) + ".Length");
+
+            int stateProgress = (int)Math.Floor(progress * cellsCount * n);
+            int completedCellsCount = stateProgress / n;
+            char currentCell = cellStates[stateProgress % n];
+
+            string completedCells = new string(cellStates[^1], completedCellsCount);
+            string emptyCells = new string(cellStates[0], cellsCount - completedCellsCount - 1);
+            return completedCells + currentCell + emptyCells;
         }
     }
 }
