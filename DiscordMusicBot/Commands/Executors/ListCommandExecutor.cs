1c4686b292a8f3070dff52b24c8cc4b10530be9a
Festino <offestashka@mail.ru>
2023-12-27 15:01:35 +0300

    feat: use current video time in list command

--- DiscordMusicBot/Commands/Executors/ListCommandExecutor.cs
+++ DiscordMusicBot/Commands/Executors/ListCommandExecutor.cs
@@ -8,26 +8,38 @@ namespace DiscordMusicBot.Commands.Executors
     {
         private readonly IMessageSender _messageSender;
         private readonly RequestQueue _queue;
+        private readonly IAudioStreamer _streamer;
 
-        public ListCommandExecutor(IMessageSender notificationService, RequestQueue queue)
+        public ListCommandExecutor(IMessageSender notificationService, RequestQueue queue, IAudioStreamer streamer)
         {
             _queue = queue;
+            _streamer = streamer;
             _messageSender = notificationService;
         }
 
         public async Task ExecuteAsync(string args, DiscordMessageInfo messageInfo)
         {
-            List<Video> list = _queue.GetVideos();
-            if (list.Count == 0)
+            List<Video> videos = _queue.GetVideos();
+            if (videos.Count == 0)
             {
                 string message1 = "queue is empty";
                 await _messageSender.SendAsync(CommandStatus.Info, message1, messageInfo);
                 return;
             }
 
-            var fullTime = TimeSpan.FromSeconds(list.Sum(v => v.Header.Duration.TotalSeconds));
-            string videoListStr = FormatUtils.FormatVideos(list.Select(v => v.Header).ToList());
-            string message = string.Format("{0} songs, {1}\n{2}", list.Count, fullTime, videoListStr);
+            var fullTime = TimeSpan.FromSeconds(videos.Sum(v => v.Header.Duration.TotalSeconds));
+
+            AudioInfo? info = _streamer.GetPlaybackInfo();
+            TimeSpan currentTime = TimeSpan.Zero;
+            if (info is not null && info.Video == videos[0])
+            {
+                currentTime = info.CurrentTime;
+            }
+            fullTime -= currentTime;
+
+            string fullTimeStr = FormatUtils.FormatTimestamp(fullTime);
+            string videoListStr = FormatUtils.FormatVideos(videos.Select(v => v.Header).ToList());
+            string message = string.Format("{0} songs, {1}\n{2}", videos.Count, fullTimeStr, videoListStr);
             await _messageSender.SendAsync(CommandStatus.Info, message, messageInfo);
         }
     }
