f2b292b42b61a1cb3dc6b778051765d480b59d3c
Festino <offestashka@mail.ru>
2024-01-08 14:29:33 +0300

    feat: add VideoHeader accessability fields

--- DiscordMusicBot/Abstractions/VideoHeader.cs
+++ DiscordMusicBot/Abstractions/VideoHeader.cs
@@ -1,4 +1,5 @@
 ﻿namespace DiscordMusicBot.Abstractions
 {
-    public record VideoHeader(string ChannelName, string Title, TimeSpan Duration);
+    public record VideoHeader(string ChannelName, string Title, TimeSpan Duration,
+                              bool Live, bool Copyright, string? ReasonUnplayable);
 }
